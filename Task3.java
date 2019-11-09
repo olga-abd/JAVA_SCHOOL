9ffae23fbbef30e1c6a450fdd20e57c9d1518b15
OLGA <o_lk_a@mail.ru>
2019-11-09 19:41:35 +0500

    updated 3,4

--- Task3.java
+++ Task3.java
@@ -5,18 +5,22 @@ public class Task3 {
 		
 		System.out.println(search1(str,srch));
 		System.out.println(search2(str,srch));
+		System.out.println(search5(str,srch));
 		System.out.println("Count: " + search3_without_positions(str,srch));
+		System.out.println("Count: " + search4_without_positions(str,srch));
 	}
 	
-	public static String search1(String str, char srch){
+	// перебор подряд по всем символам
+	public static String search1(String str, char srch){ 
 		char[] chrs = str.toCharArray();
 		StringBuffer positions = new StringBuffer();
 		int count = 0;
 		String str_out = new String();
 		
 		if (str.indexOf(srch) > -1) {
+			// проходим по каждому символу
 			for(int i = 0; i < str.length(); i++) {
-				if(chrs[i] == srch) {
+				if(chrs[i] == srch) { // если он соответствует шаблону - считаем его и записываем позицию
 					count ++;
 					positions.append(", " + i);
 				}
@@ -31,12 +35,14 @@ public class Task3 {
 		return str_out;
 	}
 	
+	// поиск через indexOf
 	public static String search2(String str, char srch){
 		StringBuffer positions = new StringBuffer();
 		int count = 0;
 		int ind = 0;
 		String str_out = new String();
 		
+		// ищем символ через indexOf. со следующей позиции после найденного символа
 		while (str.substring(ind).indexOf(srch) > -1){
 			int i = str.substring(ind).indexOf(srch);
 			ind += i;
@@ -54,10 +60,46 @@ public class Task3 {
 		}
 		return str_out;
 	}
-
+	
+	// подсчет через replaceAll
 	public static int search3_without_positions(String str, char srch) {
+		// из длины строки вычитаем длину строки без шаблонных символов. разница - количество этих символов в строке
 		String str_replaced = str.replaceAll(String.valueOf(srch),"");
 		return str.length() - str_replaced.length();
+	}
+	
+	// подсчет через replaceAll 2
+	public static int search4_without_positions(String str, char srch) {
+		// удаляем все символы, кроме шаблонного. считаем
+		return str.replaceAll("[^" + srch + "]","").length();
+	}
+	
+	// поиск через indexOf 2. обрезаем строку, когда нашли шаблон
+	public static String search5(String str, char srch){
+		StringBuffer positions = new StringBuffer();
+		int count = 0;
+		int ind = 0;
+		StringBuffer str_out = new StringBuffer(str);
+		
+		// ищем символ через indexOf 
+		while (str_out.indexOf(String.valueOf(srch)) > -1){
+			int i = str_out.indexOf(String.valueOf(srch));
+			ind += i; 
+			positions.append(", " + ind);
+			ind++;
+			count++;
+			
+			str_out.delete(0,i+1); //обрезаем строку
+		}
+		
+		if (count > 0) {
+			return ("Count: " + count + "; positions: " + positions.substring(2));
+		}
+		else
+		{
+			return ("Not found");
+		}
+		
 	}
 
 }
--- Task4.java
+++ Task4.java
@@ -1,17 +1,20 @@
 public class Task4 {
 	public static void main(String[] args){
-		String str_in = "5 * 2 - 3";
-		System.out.println (simplecalc(str_in));
+		String str_in = "( 6 + 10 - 4 ) / ( 1 + 1 * 2 ) + 1"; // всё вводить через пробелы
+		//System.out.println (simplecalc(str_in));
+		
+		System.out.println(prioritycalc(str_in));
 	}
 	
 	public static double simplecalc (String str_in){
+		
 		String[] str_arr = str_in.split(" ");
 		
 		double tmp = 0;
 		StringBuffer sgn = new StringBuffer();
 		
 		for (String s : str_arr) {
-			if (s.matches("\\d")) {
+			if (s.matches("\\d+.?\\d*")) {
 				switch (sgn.toString()) {
 					case "+":
 					{
@@ -47,4 +50,123 @@ public class Task4 {
 		}
 		return tmp;
 	}
+
+	public static double prioritycalc (String str_in){
+		String str = shunting_yard(str_in); // сортируем операции по приоритету на основе алгоритма сортировочной станции
+		
+		return polskcalc(str); // делаем расчет в соотсветствии с обратной польской нотацией
+	}
+
+	public static String shunting_yard (String str_in){
+		String[] str = str_in.split(" ");
+		
+		StringBuffer sout = new StringBuffer(); // стек вывода
+		
+		StringBuffer sop = new StringBuffer(); // стек операций
+		
+		for (String s : str) {
+			// числа записываем в стек чисел через запятую
+			if(s.matches("\\d+")) {
+				sout.append("," + s.trim());
+			}
+			else {
+				// операции - стек операций. Если приоритет новой операции больше приоритета последней операции, то последнюю переносим в стек вывода
+				if (sop.length() > 0 && !s.trim().equals("(") && !s.trim().equals(")") && sop.charAt(sop.length()-1) != '('
+						&& get_prior(sop.charAt(sop.length()-1)) <= get_prior(s.trim().charAt(0))){
+					char a;
+					a = sop.charAt(sop.length()-1);
+					sop.deleteCharAt(sop.length()-1);
+					sop.deleteCharAt(sop.length()-1);
+					sout.append("," + a);
+					}
+				else if (s.trim().equals(")")) {
+					char a; // все в пределах скобок переносим в стек вывода
+					while(sop.charAt(sop.length()-1) != '('){
+						a = sop.charAt(sop.length() - 1);
+						sop.deleteCharAt(sop.length() - 1);
+						sop.deleteCharAt(sop.length() - 1);
+						sout.append("," + a);
+					}
+					sop.deleteCharAt(sop.length() - 1);
+					sop.deleteCharAt(sop.length() - 1);
+				}
+				if(!s.trim().equals(")")) sop.append("," + s.trim());
+				
+			}
+			
+		}
+		while (sop.length() > 1) {
+			char a; // оставшиеся операции переносим в стек вывода
+			a = sop.charAt(sop.length() - 1);
+			sop.deleteCharAt(sop.length() - 1);
+			sop.deleteCharAt(sop.length() - 1);
+			sout.append("," + a);
+		}
+		sout.deleteCharAt(0); // удаляем начальную запятую
+		return String.valueOf(sout);		
+	}
+	
+	public static double polskcalc(String str_in){
+		StringBuffer str = new StringBuffer(str_in);
+		
+		int arr_len = str_in.replaceAll("[^,]+","").length(); // длину массива определяем по количеству запятых в стеке
+		String[] sout = new String[arr_len];
+		int i = 0;
+		
+		while (str.length() > 0) {
+			
+			String a = new String();
+			
+			// получаем значение от начала до первой запятой. или до конца строки, если запятой больше нет
+			if(str.indexOf(",") > -1){
+				a = String.valueOf(str.substring(0,str.indexOf(",")));
+			}
+			else {
+				a = String.valueOf(str);
+			}
+			
+			if (a.matches("\\d+")) {
+				sout[i++] = a;
+				// если число - записываем в массив
+			}
+			else {
+				i--;
+				// если оператор - производим вычисление по последним записанным в массив числам
+				double b = simplecalc(sout[i-1] + " " + a + " " + sout[i]);
+				sout[i--] = null;
+				sout[i++] = String.valueOf(b);
+			}
+			
+			// убираем обработанные числа/операции
+			if (str.indexOf(",") > -1) 
+				str.delete(0,str.indexOf(",")+1);
+			else str.setLength(0);
+			
+			/*for(String s : sout){
+				System.out.print(s + " ");
+			}*/
+		}
+		
+		return Double.valueOf(sout[0]);
+	}
+	
+	// определяем приоритет операторов
+	public static int get_prior (char pr) { 
+		int tmp;
+		switch (pr) {
+			case '*':
+			case '/':
+			{
+				tmp = 1;
+				break;
+			}
+			default:
+			{
+				tmp = 2;
+				break;
+			}
+		}
+		return tmp;
+	}
+
 }
